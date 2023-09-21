1bea4b1d45065db7177cea525fc8a90a43b81b0b
ElordeCatabay <elgu.catabay.up@phinmaed.com>
2023-09-21 22:46:20 +0800

    first commit

---
+++ src/EnglishQuestions.js
@@ -0,0 +1,71 @@
+export const englishData = [
+  {
+    question: "Anong masarap na kape?",
+    Options: ['kopiko', 'starbucks', 'americano', 'black coffee'],
+    correct: 1,
+    marked: -1,
+  },
+  {
+    question: "Edi Kapeling ka",
+    Options: ['on account of', 'due', 'because', 'owing'],
+    correct: 2,
+    marked: -1,
+  },
+  {
+    question: "HAHAHAHA",
+    Options: [
+      'not having said',
+      'have never said',
+      'never said',
+      'had never said',
+    ],
+    correct: 1,
+    marked: -1,
+  },
+  {
+    question: 'Kupal.',
+    Options: [
+      'to be abducted',
+      'to be abducting',
+      'to have been abducted',
+      'to have been abducting',
+    ],
+    correct: 3,
+    marked: -1,
+  },
+  {
+    question: 'AHAHAHAHAHA',
+    Options: ['herself', 'her', 'her own', 'hers'],
+    correct: 2,
+    marked: -1,
+  },
+  {
+    question: 'ANIMAL KA',
+    Options: [
+      'Not wanting',
+      'As not wanting',
+      "She didn't want",
+      'Because not wanting',
+    ],
+    correct: 4,
+    marked: -1,
+  },
+  {
+    question: "fishball",
+    Options: [
+      "There's no point",
+      "It's no point",
+      "There isn't point",
+      "It's no need",
+    ],
+    correct: 4,
+    marked: -1,
+  },
+  {
+    question:
+      'lima',
+    Options: ['had written', 'has written', 'had been writing', 'wrote'],
+    correct: 1,
+    marked: -1,
+  },
+];
